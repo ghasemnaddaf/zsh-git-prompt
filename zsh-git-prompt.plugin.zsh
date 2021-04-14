a077a48df3db9590b9c422b3c78407770e27913f
Ghasem Shirazi <gshirazi@infoblox.com>
2021-04-14 12:32:25 -0700

    make this a plugin

---
+++ zsh-git-prompt.plugin.zsh
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------------------------------
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+# -------------------------------------------------------------------------------------------------
+
+
+# Standarized way of handling finding plugin dir,
+# regardless of functionargzero and posixargzero,
+# and with an option for a plugin manager to alter
+# the plugin directory (i.e. set ZERO parameter)
+# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+source ./zshrc.sh
+PROMPT='%B%m%~%b$(git_super_status) %# '
+
